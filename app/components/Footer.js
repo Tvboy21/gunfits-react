3c733e393c672f21d227f685f1d3ec19b4a30706
Tvboy21 <adrianogalo254@gmail.com>
2026-08-20 12:00:11 +0300

    Update Instagram link

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,11 +6,11 @@ export default function Footer() {
           <span style={{color: '#C94E0A'}}>GUN</span>FITS
         </div>
         <ul className="footer-links">
-          <li><a href="#">Instagram</a></li>
-          <li><a href="#">TikTok</a></li>
-          <li><a href="#">Shipping</a></li>
-          <li><a href="#">Returns</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="https://www.instagram.com/gun_fits?igsh=MWJ0emw4ZncwY2FqOA==" target="_blank" rel="noreferrer">Instagram</a></li>
+          <li><a href="https://www.tiktok.com/@gunfits" target="_blank" rel="noreferrer">TikTok</a></li>
+          <li><a href="/collections">Shop</a></li>
+          <li><a href="mailto:hello@gunfits.co">Contact</a></li>
+          <li><a href="mailto:hello@gunfits.co?subject=Return%20Query">Returns</a></li>
         </ul>
         <p className="footer-copy">EST 2021.</p>
       </div>
